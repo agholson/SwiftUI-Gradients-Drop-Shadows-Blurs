2dcdd2d065ea9ac8c3cd33b7654c6b4450a4fc6a
agholson <87455899+agholson@users.noreply.github.com>
2022-03-05 09:31:53 -0500

    Initial Commit

---
+++ M1L4 GradientsDropShadowsBlurs/M1L4_GradientsDropShadowsBlursApp.swift
@@ -0,0 +1,17 @@
+//
+//  M1L4_GradientsDropShadowsBlursApp.swift
+//  M1L4 GradientsDropShadowsBlurs
+//
+//  Created by Leone on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct M1L4_GradientsDropShadowsBlursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
